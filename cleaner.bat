806a01a60ada8bd7c81a6ddfc2aa3b49a7037470
Qardruss <78308435+Qardruss@users.noreply.github.com>
2021-09-26 10:17:40 +0100

    Update cleaner.bat

--- cleaner.bat
+++ cleaner.bat
@@ -8,13 +8,15 @@ md c:\windows\temp
 del /s /f /q %SYSTEMDRIVE%\WINDOWS\Prefetch
 del /s /f /q %temp%\*.*
 rd /s /q %temp%
-
+echo Done cleaning temporary files
+echo Cleaning logs
 md %temp%
 del /q /f /s %SYSTEMDRIVE%\Temp\*.*
 del /q /f /s %WINDIR%\Prefetch\*.*
 del /q /f /s %SYSTEMDRIVE%\*.log
 del /q /f /s %SYSTEMDRIVE%\*.bak
 del /q /f /s %SYSTEMDRIVE%\*.gid
-echo Done cleaning temporary files
+echo Cleaned logs
+echo Thanks for using Pc Cleaner
 pause
 cls
